225d942ca566492d19e0d36271e198ddc8244795
Christian Panse <cp@fgcz.ethz.ch>
2019-07-22 16:11:26 +0200

    initial

---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+#R
+
+.onAttach <- function(lib, pkg) {
+    if (interactive()) {
+        version <- packageVersion('tartare')
+        packageStartupMessage("Package 'tartare' version ", version)
+        
+        invisible()
+    }
+}
+
+
+
+#' getExperimentHubFilename
+#'
+#' @param filename of the aws s3 blob.
+#' @return the file name of the local ExperimentHub.
+#' 
+#' @export getExperimentHubFilename
+#' @importFrom AnnotationHub query
+#' @importFrom ExperimentHub ExperimentHub
+#' 
+#' @examples 
+#' fl <- system.file("extdata", "metadata.csv", package="tartare")   
+#' metadata <- read.csv(fl, stringsAsFactors=FALSE)
+#'      metadata$Title     
+#'      
+#' lapply(metadata$RDataPath, getExperimentHubFilename)
+getExperimentHubFilename <- function(filename){
+   suppressMessages({
+        eh = ExperimentHub()
+    
+        filename <- query(eh, c("tartare", filename))[[1]]
+       })
+    filename
+}
